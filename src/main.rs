5d74b9da744f151caebb765eac3999f386c4e9aa
DocCreeps <dorian1998dino@hotmail.com>
2024-03-02 14:27:46 +0100

    add json db

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,83 @@
 //Un programme pour gérer une liste de tâches à faire
 //Il doit pouvoir ajouter, lister et marquer des tâches comme faites
-
 use std::collections::HashMap;
-use std::io::Read;
-use std::str::FromStr;
 
+fn main() {
+    // Récupère l'action à effectuer depuis les arguments de la ligne de commande
+    let action = std::env::args().nth(1).expect("Veuillez fournir une action");
+    // Récupère l'élément associé à l'action depuis les arguments de la ligne de commande
+    let item = std::env::args().nth(2).expect("Veuillez fournir un élément");
+
+    // Initialise une instance de Todo et gestion des erreurs si l'initialisation échoue
+    let mut todo = Todo::new().expect("L'initialisation de la base de données a échoué");
+
+    // Vérifie l'action et effectue les opérations correspondantes
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("Tâche enregistrée"),
+            Err(why) => println!("Une erreur s'est produite : {}", why),
+        }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' n'est pas présent dans la liste", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("Tâche enregistrée"),
+                Err(why) => println!("Une erreur s'est produite : {}", why),
+            },
+        }
+    }
+}
 
 struct Todo {
-    //On utilise une HashMap pour stocker les tâches
+    // Utilise HashMap intégré à Rust pour stocker des paires clé-valeur
     map: HashMap<String, bool>,
 }
 
 impl Todo {
-    //Une méthode pour ajouter une tâche à la liste
-    fn insert(&mut self, key: String) {
-        self.map.insert(key, false);
-    }
-
-    //Une méthode pour stocker les tâches dans un fichier
-    fn save(self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k, v) in self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record)
-        }
-        std::fs::write("db.txt", content)
-    }
-
-
     fn new() -> Result<Todo, std::io::Error> {
-        // Ouvre le fichier "db.txt" en mode écriture, création et lecture
-        let mut f = std::fs::OpenOptions::new()
+        // Ouvre le fichier "db.json" en écriture, création et lecture
+        let f = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
-            .open("db.txt")?;
+            .open("db.json")?;
 
-        // Initialise une chaîne de caractères pour stocker le contenu du fichier
-        let mut content = String::new();
-
-        // Lit le contenu du fichier dans la chaîne de caractères
-        f.read_to_string(&mut content)?;
-
-        // Transforme le contenu en une HashMap<String, bool>
-        let map: HashMap<String, bool> = content
-            .lines()
-            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
-            .map(|v| (v[0], v[1]))
-            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
-            .collect();
-
-        // Retourne un résultat contenant la structure Todo avec la HashMap
-        Ok(Todo { map })
+        // Tente de désérialiser le contenu du fichier en HashMap
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }),
+            // Gère le cas où le fichier est vide en initialisant une nouvelle HashMap
+            Err(e) if e.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            // Gère les autres erreurs de désérialisation de manière paniquante
+            Err(e) => panic!("Une erreur s'est produite : {}", e),
+        }
     }
 
-    //Une méthode pour marquer une tâche comme faite
+    fn insert(&mut self, key: String) {
+        // Insère un nouvel élément dans notre HashMap, l'état actif est défini par défaut à vrai
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        // Ouvre le fichier "db.json" en écriture, création et troncature
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open("db.json")?;
+        // Sérialise et écrit joliment la HashMap dans le fichier
+        serde_json::to_writer_pretty(f, &self.map)?;
+
+        Ok(())
+    }
+
     fn complete(&mut self, key: &String) -> Option<()> {
+        // Marque l'élément correspondant comme complet s'il existe dans la HashMap
         match self.map.get_mut(key) {
-            Some(v) => Some(*v = true),
+            Some(v) => Some(*v = false),
             None => None,
-        }
-    }
-
-}
-fn main() {
-    let mut todo = Todo::new().expect("Initialisation de la db échoué");
-    let action = std::env::args().nth(1).expect("Renseignez une action");
-    let item = std::env::args().nth(2).expect("Renseignez un item");
-
-    if action == "add" {
-        todo.insert(item);
-        match todo.save() {
-            Ok(_) => println!("todo sauvegarder"),
-            Err(why) => println!("Une erreur à été rencontrer: {}", why),
-        }
-    }
-    else if action == "complete" {
-        match todo.complete(&item) {
-            None => println!("'{}' n'est pas prèsent dans la list", item),
-            Some(_) => match todo.save() {
-                Ok(_) => println!("todo sauvegarder"),
-                Err(why) => println!("Une erreur à été rencontrer: {}", why),
-            },
         }
     }
 }
